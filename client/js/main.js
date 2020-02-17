f9805b26ec8ab6d34f2bebf384b5cf93710a71ce
Chen <luchenrong1@gmail.com>
2020-02-17 23:53:36 +1300

    shalom

--- client/js/main.js
+++ client/js/main.js
@@ -1,5 +1,12 @@
 var mode = "show";
-var progress = 4000;
+var progress = 100;
+var slideprogress = 0;
+var contentprogress =0 ;
+var slideup = false;
+var contenttype = "";
+var prevcontent = "";
+var curtab = ""
+var animating = false;
 document.onkeyup = function(e){
     mode = "show";
 }
@@ -32,17 +39,71 @@ setInterval(function(){
 
 function showtab(tab){
     switch(tab) {
-        case "About":
-          // code block
+        case "About": 
+        if(animating){
+            break;
+        }
+        if(curtab!="About"){
+            contentprogress = 0;    
+            prevcontent = contenttype;
+            contenttype = "about";
+            window.requestAnimationFrame(slideleftnav);    
+        }
+        curtab = "About"
           break;
         case "Opening":
-          // code block
+                if(animating){
+                    break;
+                }
+        if(curtab!="Opening"){
+        contentprogress = 0;    
+        prevcontent = contenttype;
+        contenttype = "opening";
+        window.requestAnimationFrame(slideleftnav);    
+        }
+        curtab = "Opening"
           break;
         case "News":
-            console.log("news");
+ 
         case "Contact":
+                            
+
 
         default:
           // code block
       } 
+}
+function slideleftnav(){
+    if(slideup){
+        if(prevcontent!=""){
+            document.getElementById(prevcontent).style.display = "none";
+        }
+        document.getElementById(contenttype).style.display = "block";       
+        window.requestAnimationFrame(showcontent);        
+        return;
+    }
+    if(slideprogress<100){
+        document.getElementById("navmain").style["margin-top"] = (20*(1-slideprogress/105)).toString() + "%";           
+        slideprogress +=1;
+        window.requestAnimationFrame(slideleftnav);
+    }else{        
+        slideup = true;
+        if(prevcontent!=""){
+            document.getElementById(prevcontent).style.display = "none";
+        }
+        document.getElementById(contenttype).style.display = "block";       
+        window.requestAnimationFrame(showcontent);                
+        return;
+    }
+}
+function showcontent(){    
+    if(contentprogress<100){
+        document.getElementById(contenttype).style["opacity"] = (contentprogress/99);             
+    contentprogress +=1;
+    window.requestAnimationFrame(showcontent);        
+    }else{     
+        contentprogress = 0;    
+        animating = false;
+        return;
+    }
 }
